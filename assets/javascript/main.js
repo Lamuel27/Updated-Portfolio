f9846b27ee217b072999353ad7856e9488d61faa
Lamuel27 <samrelliner@gmail.com>
2018-11-15 09:44:55 -0600

    initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
+
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+// var instance = M.Carousel.getInstance();
+
+// instance.next();
+// instance.next(3); // Move next n times.
+// instance.prev();
+// instance.prev(3); // Move previous n times.
+// instance.set();
+// instance.set(3); // Set to nth slide.
+// instance.destroy();
+//   // Next slide
+// $('.carousel').carousel('next');
+// $('.carousel').carousel('next', 3); // Move next n times.
+
+// // Previous slide
+// $('.carousel').carousel('prev');
+// $('.carousel').carousel('prev', 4); // Move prev n times.
+
+// // Set to nth slide
+// $('.carousel').carousel('set', 4);
+
+// // Destroy carousel
+// $('.carousel').carousel('destroy');
+
